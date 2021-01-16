ea90c8f967bca83ad5f99d57a028ad4c11f66482
Nadav Frank <frankos333@gmail.com>
2021-01-16 23:51:33 +0200

    test

--- src/components/BoardPreview.jsx
+++ src/components/BoardPreview.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const BoardPreview = ({ boardNumber }) => {
     return (
         <div className="board-preview-container">
+            <h2>just for testing</h2>
             <h2> Board { boardNumber } </h2>
         </div>
     )
